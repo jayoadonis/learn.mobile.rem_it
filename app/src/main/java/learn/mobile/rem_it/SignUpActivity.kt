90c5ef2e82a1608a6dee739f42c3428047d6f713
jayoadonis <jayo.adonisraphael@gmail.com>
2025-02-01 09:28:51 +0800

    [chore: 0.0.0] .|. Add password visible toggle

--- app/src/main/java/learn/mobile/rem_it/SignUpActivity.kt
+++ app/src/main/java/learn/mobile/rem_it/SignUpActivity.kt
@@ -3,6 +3,8 @@ package learn.mobile.rem_it
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.content.res.AppCompatResources
+import com.google.android.material.textfield.TextInputLayout
 
 
 import learn.mobile.rem_it.databinding.ActivitySignUpBinding;
@@ -23,5 +25,24 @@ class SignUpActivity : AppCompatActivity() {
             super.startActivity( Intent(this, MainActivity::class.java) );
             super.finish();
         }
+
+//        val passwordInputLayout = this.signUpActivityBind.txtInputLayoutPassword;
+//        val confirmPasswordInputLayout = this.signUpActivityBind.txtInputLayoutConfirmPassword;
+
+//        passwordInputLayout.setEndIconOnClickListener {
+//            val isVisible = passwordInputLayout.endIconDrawable?.constantState?.equals(
+//                AppCompatResources.getDrawable(
+//                    this.applicationContext,
+//                    com.google.android.material.R.drawable.ic_call_answer
+//                )?.constantState
+//            ) ?: false
+//
+//            if (isVisible) {
+//                confirmPasswordInputLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+//            } else {
+//                confirmPasswordInputLayout.endIconMode = TextInputLayout.END_ICON_NONE
+//            }
+//        }
+
     }
 }
